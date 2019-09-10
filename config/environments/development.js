5271c642bb38b4e37869c3e644b1af3eca8788be
Naomy Rios <zoey.rios@unitec.edu>
2019-09-09 22:40:54 -0600

    Initial commit

---
+++ config/environments/development.js
@@ -0,0 +1,11 @@
+module.exports = {
+  PORT: process.env.PORT,
+  DB: {
+    username: "opqyvypc",
+    password: "dYhQwrUcayeyBKwusM0CD3T3wcQIt_Sj",
+    database: "opqyvypc",
+    host: "salt.db.elephantsql.com",
+    dialect: "postgres",
+    logging: false
+  }
+};
